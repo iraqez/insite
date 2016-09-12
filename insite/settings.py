dc999f79c994054da3d98bc32973587952cdff4a
Iraqez <vladimir.gumenyuk@gmail.com>
2016-09-12 20:25:26 +0300

    1

--- insite/settings.py
+++ insite/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ADMINS = [('iraqez', 'v.gumenyuk@agro2012.com.ua')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
--- instr/admin.py
+++ instr/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
+#from django.contrib.admin.sites import site, AdminSite
 from .models import Subdivision, Leading, Docs
 
 
-# @admin.register(Subdivision, Leading, Docs)
+# @admin.register(Subdivision, Leading,)
 
-# class DocsInLine(admin.):
-#     model = Docs
-@admin.register(Subdivision, Leading)
+class DocsInLine(admin.StackedInline):
+    model = Docs
+
 
 class SubdivisionAdmin(admin.ModelAdmin):
     None
 
 
 class LeadingAdmin(admin.ModelAdmin):
-    None
-    # inlines = [ DocsInLine ]
+    inlines = [ DocsInLine ]
+    #None
+
+admin.site.register(Leading, LeadingAdmin)
