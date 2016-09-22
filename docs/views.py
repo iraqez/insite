ffc10cc8aba12a88a7431141846b7a7e2f5b82be
iraqez <vladimir.gumenyuk@gmail.com>
2016-09-22 10:05:20 +0300

    123

--- docs/views.py
+++ docs/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 from .models import Subdivision
 
+
+# from .models import LeadingInstrukciy, LeadingPolozhennya, LeadingProcedure
+# from .models import DocsInstrukciy, DocsPolozhennya, DocsProcedure
+
 def index(request):
     latest_subdivisions_list = Subdivision.objects.all()
-    context = { 'latest_subdivisions_list': latest_subdivisions_list }
-    return render(request, 'instr/index.html', context)
+    context = {'latest_subdivisions_list': latest_subdivisions_list}
+    return render(request, 'docs/index.html', context)
--- insite/settings.py
+++ insite/settings.py
@@ -89,6 +89,19 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': 'insite_vapg',
+#     'USER': 'postgres',
+#     'PASSWORD': 'workfree',
+#     'HOST': 'localhost', # Set to empty string for localhost.
+#     'PORT': '5432', # Set to empty string for default.
+#     }
+# }
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
