74c821c1acb7361e41274f2805f2785a2567a474
iraqez <vladimir.gumenyuk@gmail.com>
2016-09-22 10:19:44 +0300

    123

--- insite/settings.py
+++ insite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_cleanup',
     'tinymce',
     'docs',
+    'main',
 ]
 
 MIDDLEWARE_CLASSES = [
--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-
 def MainPageView(request):
-    return HttpResponse("<a href='http://in.vapg.com.ua/instrukciy/'>Посадові інструкції</a>")
+    return render(request, 'redirect/index.html')
