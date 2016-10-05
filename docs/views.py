29f8568ede0416942624c663a9ce901ca4848cf2
iraqez <vladimir.gumenyuk@gmail.com>
2016-10-05 18:31:26 +0300

    d

--- docs/views.py
+++ docs/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+
 from .models import Subdivision
-
-
-# from .models import LeadingInstrukciy, LeadingPolozhennya, LeadingProcedure
+from .models import LeadingInstrukciy, LeadingPolozhennya, LeadingProcedure
 # from .models import DocsInstrukciy, DocsPolozhennya, DocsProcedure
 
-def index(request):
-    latest_subdivisions_list = Subdivision.objects.all()
-    context = {'latest_subdivisions_list': latest_subdivisions_list}
-    return render(request, 'docs/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'docs/category_list.html'
+    context_object_name = 'latest_subdivisions_list'
+    queryset = Subdivision.objects.all()
+
+class InstrukciyView(generic.ListView):
+    template_name = 'docs/documents_list.html'
+    # context_object_name = 'latest_instr_list'
+    # queryset = LeadingInstrukciy.objects.filter(subdivision__title='IT')
+
+    def get_queryset(self):
+        self.subdivision = get_object_or_404(Subdivision, name=self.args[1])
+        return LeadingInstrukciy.objects.filter(subdivision=self.subdivision)
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get a context
+    #     context = super(InstrukciyView, self).get_context_data(**kwargs)
+    #     # Add in the publisher
+    #     context['subdivision'] = self.sub
+    #     return context
+
+
