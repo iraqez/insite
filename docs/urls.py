dcc1b553744ed62c343931e4834cd016ed97b347
Iraqez <vladimir.gumenyuk@gmail.com>
2016-10-06 10:17:22 +0300

    12

--- docs/urls.py
+++ docs/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+from docs.views import SubdivisionInstrView
 
 app_name = 'docs'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'(?P<subdivision_slug>\w+)/', views.InstrukciyView.as_view(), name='instr'),
+    # url(r'(?P<subdivision_slug>\w+)/', views.InstrukciyView.as_view(), name='instr'),
+    url(r'(?P<subdivision_slug>\w+)/', SubdivisionInstrView.as_view(), name='instr'),
 ]
--- docs/views.py
+++ docs/views.py
@@ -17,14 +17,14 @@ class IndexView(generic.ListView):
 class InstrukciyView(generic.ListView):
     template_name = 'docs/documents_list.html'
     context_object_name = 'latest_instr_list'
-    queryset = LeadingInstrukciy.objects.filter(subdivision__title='IT')
+    queryset = LeadingInstrukciy.objects.get
 
+class SubdivisionInstrView(generic.ListView):
+    template_name = 'docs/documents_list.html'
 
-    # def get_context_data(self, **kwargs):
-    #     # Call the base implementation first to get a context
-    #     context = super(InstrukciyView, self).get_context_data(**kwargs)
-    #     # Add in the publisher
-    #     context['subdivision'] = self.sub
-    #     return context
+    def get_queryset(self):
+        self.subdivision = get_object_or_404(Subdivision, slug=self.args[0])
+        return LeadingInstrukciy.objects.filter(subdivision=self.subdivision)
+
 
 
