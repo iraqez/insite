f68e28265209db29ae451cc95863c93a43b8d819
Iraqez <vladimir.gumenyuk@gmail.com>
2016-10-18 09:08:34 +0300

    12

--- docs/urls.py
+++ docs/urls.py
@@ -9,6 +9,6 @@ app_name = 'docs'
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
   #  url(r'^(?P<slug>)', LeadingView.as_view(), name='leading_detail'),
-  #  url(r'^(?P<slug>)', TemplateView.as_view(template_name='docs/test.html'), name='test'),
+    #url(r'^test', TemplateView.as_view(template_name='docs/test.html'), name='test'),
     url(r'^(?P<subdivision_slug>)', SubdivisionView.as_view(), name='instr'),
 ]
